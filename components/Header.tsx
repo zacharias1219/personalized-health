1a6da62dd115b3522c0ff9c04a715621ac55ff29
Richard Abishai <rickoshade1891@gmail.com>
2024-11-07 11:32:38 +0530

    mini changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,12 +7,12 @@ const Header = () => {
     <header className='border bg-black border-black'>
         <div className='flex flex-row text-white justify-between px-4'>
           <Link href="/">
-            <section className='flex justify-between items-center gap-4 p-4'>
+            <section className='flex justify-between items-center gap-2 p-3 md:gap-4 md:p-4'>
                 <Image src='/logo.svg' width={30} height={30} alt='logo' className='object-contain'/>
-                <div className='text-3xl font-bold'>Heal</div>
+                <div className='text-2xl md:text-3xl font-bold'>Heal</div>
             </section>
           </Link>
-            <section className='flex justify-between gap-6 items-center p-4'>
+            <section className='flex justify-between gap-4 md:gap-6 items-center p-4'>
                 <Link href="/#"><div className='text-xl font-bold'>About</div></Link>
                 <Link href="/#"><div className='text-xl font-bold'>Contact</div></Link>
                 <Link href="/#"><div className='text-xl font-bold'>Blogs</div></Link>
